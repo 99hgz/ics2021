8b9458d2f6bbaa62f75ad9ba2e11aec76a865ac2
tracer-ics2021 <tracer@njuics.org>
2022-07-08 20:21:01 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  20:21:01 up  4:09,  1 user,  load average: 0.17, 0.09, 0.02

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -5,7 +5,7 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {m
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
