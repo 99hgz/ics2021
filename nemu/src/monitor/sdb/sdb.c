4ef6c5729da9053a627020724333aa4544364dbe
tracer-ics2021 <tracer@njuics.org>
2022-07-09 12:01:13 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  12:01:13 up 19:49,  0 users,  load average: 0.11, 0.06, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -37,6 +37,11 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  cpu_exec(args[0]);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -47,6 +52,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Run the program for n steps", cmd_si },
 
   /* TODO: Add more commands */
 
