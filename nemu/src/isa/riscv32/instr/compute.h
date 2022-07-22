2a7b77ff772591c32916256d94408018380bf61d
tracer-ics2021 <tracer@njuics.org>
2022-07-22 13:16:41 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  13:16:41 up 13 days, 21:04,  0 users,  load average: 0.17, 0.11, 0.10

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -24,6 +24,31 @@ def_compute_ehelperi(sll)
 def_compute_ehelperi(srl)
 def_compute_ehelperi(sra)
 
+def_EHelper(mul) {
+  rtl_mulu_lo(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(mulh) {
+  rtl_muls_hi(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(mulsu) {
+  rtl_mulsu_hi(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(mulu) {
+  rtl_mulu_hi(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(div) {
+  rtl_divs_q(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(divu) {
+  rtl_divu_q(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(rem) {
+  rtl_divs_r(s, ddest, dsrc1, dsrc2);
+}
+def_EHelper(remu) {
+  rtl_divu_r(s, ddest, dsrc1, dsrc2);
+}
+
 def_EHelper(slt) {
   rtl_li(s, ddest, interpret_relop(RELOP_LT,*dsrc1,*dsrc2));
 }
@@ -71,4 +96,12 @@ def_EHelper(blt) {
 def_EHelper(bge) {
   rtl_addi(s, s0, &gpc, id_dest->imm);
   rtl_jrelop(s, RELOP_GE, dsrc1, dsrc2, *s0);
+}
+def_EHelper(bltu) {
+  rtl_addi(s, s0, &gpc, id_dest->imm);
+  rtl_jrelop(s, RELOP_LTU, dsrc1, dsrc2, *s0);
+}
+def_EHelper(bgeu) {
+  rtl_addi(s, s0, &gpc, id_dest->imm);
+  rtl_jrelop(s, RELOP_GEU, dsrc1, dsrc2, *s0);
 }
