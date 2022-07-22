ab2c0fb8fd46fa33353358b025d00bcb67522640
tracer-ics2021 <tracer@njuics.org>
2022-07-22 13:32:38 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  13:32:38 up 13 days, 21:20,  1 user,  load average: 0.06, 0.19, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -104,7 +104,7 @@ void cpu_exec(uint64_t n) {
   for (;n > 0; n --) {
     fetch_decode_exec_updatepc(&s);
     g_nr_guest_instr ++;
-    printf("g_nr_guest_instr = %d\n",g_nr_guest_instr);
+    printf("g_nr_guest_instr = %ld\n",g_nr_guest_instr);
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
