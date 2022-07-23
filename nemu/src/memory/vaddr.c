7c44f9734d56bb19afb6adf2c2762820fb948ab4
tracer-ics2021 <tracer@njuics.org>
2022-07-23 10:58:18 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  10:58:18 up 14 days, 18:46,  0 users,  load average: 0.01, 0.03, 0.00

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -6,7 +6,9 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  word_t tmp = paddr_read(addr, len);
+  printf("read memory address = 0x%08x data = %d = %u = %08x\n",addr,tmp,tmp,tmp);
+  return tmp;
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
