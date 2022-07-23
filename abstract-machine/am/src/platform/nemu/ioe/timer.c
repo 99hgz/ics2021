69945f3175521fbf1f790718603134395e5fc540
tracer-ics2021 <tracer@njuics.org>
2022-07-23 23:43:25 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  23:43:25 up 15 days,  7:31,  1 user,  load average: 0.00, 0.04, 0.03

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  //uptime->us = ((uint64_t)inl(0xa000004d)<<32)|inl(0xa000004e);  
+  uptime->us = ((uint64_t)inl(0xa0000048)<<32)|inl(0xa000004c);   
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
