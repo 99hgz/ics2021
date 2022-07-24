d3531dda5c86e2d478b0bdf457e65a42c77701a1
tracer-ics2021 <tracer@njuics.org>
2022-07-24 18:02:59 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  18:02:59 up 16 days,  1:51,  1 user,  load average: 0.23, 0.20, 0.11

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -66,7 +66,7 @@ static def_DHelper(R) {
 static def_DHelper(B) {
   decode_op_r(s, id_src1, s->isa.instr.b.rs1, false);
   decode_op_r(s, id_src2, s->isa.instr.b.rs2, false);
-  sword_t simm = SEXT32((s->isa.instr.b.imm4_1 << 1) | (s->isa.instr.b.imm11 << 11) | (s->isa.instr.b.imm10_5 << 5) | (s->isa.instr.b.imm12 << 12),12);
+  sword_t simm = SEXT32((s->isa.instr.b.imm4_1 << 1) | (s->isa.instr.b.imm11 << 11) | (s->isa.instr.b.imm10_5 << 5) | (s->isa.instr.b.imm12 << 12),13);
   //printf("B-type imm=%d\n",simm);
   decode_op_i(s, id_dest, simm, false);
 }
