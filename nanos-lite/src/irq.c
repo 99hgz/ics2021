7feb41032e20a136ab1452365aa7bc7309887d61
tracer-ics2021 <tracer@njuics.org>
2022-08-01 17:52:10 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: 14e67b17bd6c4c86a7fc69d0caa24107     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  17:52:11 up 24 days,  1:40,  0 users,  load average: 0.08, 0.05, 0.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,10 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
+  printf("pc=%u cause=%u status=%u",c->mepc,c->mcause,c->mcause);
+  for(int i=0;i<32;i++)
+    printf("%u ",c->gpr[i]);
+  printf("\n");
   switch (e.event) {
     default: panic("Unhandled event ID = %d", e.event);
   }
