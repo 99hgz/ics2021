18e67e14841a955880f0b5fd2798a765a7f534a9
tracer-ics2021 <tracer@njuics.org>
2022-08-04 17:59:28 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: c89ba84bbc1546acad7fb0220fd0d4f7     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  17:59:28 up 2 days,  4:09,  0 users,  load average: 0.05, 0.08, 0.06

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,31 @@
 #include <common.h>
 #include "syscall.h"
+
+int _write(int fd , const char * buf , size_t count ){
+  int cnt=0;
+  if(fd==1 || fd==2){
+    while((++cnt)<=count){
+      putch(*buf);
+      buf++;
+    }
+    return count;
+  }else{
+    return -1;
+  }
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   printf("do_syscall %d\n",a[0]);
   switch (a[0]) {
     case SYS_yield:yield();c->GPRx=0;break;
     case SYS_exit:halt(0);break;
+    case SYS_write:c->GPRx=_write(a[1],(char *)a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
