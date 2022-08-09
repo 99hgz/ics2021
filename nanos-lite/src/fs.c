ab158c11d57c13e10bd1c7d4f038eb1b586595c4
tracer-ics2021 <tracer@njuics.org>
2022-08-09 18:08:00 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: bbf9cdd0b3fc4f85802668645a0c7bd6     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  18:08:00 up 4 days, 18:17,  0 users,  load average: 0.14, 0.08, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,6 +9,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -30,6 +31,47 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
+
+int fs_open(const char *pathname, int flags, int mode){
+  int len = sizeof(file_table)/sizeof(file_table[0]);
+  for(int i=0;i<len;i++)
+    if(strcmp(pathname,file_table[i].name)==0)
+      return i;
+  panic("file not found");
+  return -1;
+}
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t fs_read(int fd, void *buf, size_t len){
+  if(file_table[fd].open_offset + len > file_table[fd].size)
+    len = file_table[fd].size - file_table[fd].open_offset;
+  size_t ret = ramdisk_read(buf, file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+  return ret;
+}
+
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len){
+  if(file_table[fd].open_offset + len > file_table[fd].size)
+    len = file_table[fd].size - file_table[fd].open_offset;
+  return ramdisk_write(buf, file_table[fd].open_offset, len);
+}
+size_t fs_lseek(int fd, size_t offset, int whence){
+  switch(whence){
+    case SEEK_SET:
+      file_table[fd].open_offset  = offset;break;
+    case SEEK_CUR:
+      file_table[fd].open_offset += offset;break;
+    case SEEK_END:
+      file_table[fd].open_offset  = file_table[fd].size - offset;break;
+    default:
+      return -1;
+  }
+  return 0;
+}
+int fs_close(int fd){
+  return 0;
+}
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,12 +10,16 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+size_t fs_read(int fd, void *buf, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_header;
   Elf_Phdr prog_header;
-  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+  int fd = fs_open(filename,0,0);
+  fs_read(fd, &elf_header, sizeof(Elf_Ehdr));
   if (memcmp(elf_header.e_ident, "\177ELF", 4))
     assert(0);
   Elf32_Off prog_header_offset = elf_header.e_phoff;
@@ -23,10 +28,12 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   uint32_t prog_idx = 0;
   while (prog_idx < elf_header.e_phnum){
     memset(&prog_header, 0, prog_header_size);
-    ramdisk_read(&prog_header, prog_header_offset, prog_header_size);
+    fs_lseek(fd, prog_header_offset, SEEK_SET);
+    fs_read(fd, &prog_header, prog_header_size);
     if (PT_LOAD == prog_header.p_type){
       //printf("loadto %u\n",prog_header.p_vaddr);
-      ramdisk_read((void *)prog_header.p_vaddr, prog_header.p_offset, prog_header.p_filesz);
+      fs_lseek(fd, prog_header.p_offset, SEEK_SET);
+      fs_read(fd, (void *)prog_header.p_vaddr, prog_header.p_filesz);
       memset((void *)(prog_header.p_vaddr+prog_header.p_filesz), 0, prog_header.p_memsz-prog_header.p_filesz);
     }
     prog_header_offset += elf_header.e_phentsize;
