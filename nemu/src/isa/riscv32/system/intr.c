87a7d30aa4f9dabe21856a873b80f0b3511f416e
tracer-ics2021 <tracer@njuics.org>
2022-08-09 19:44:24 +0800

    >  compile 201220000    Static hostname: iZj6cftv4731g6ium2xdtyZ          Icon name: computer-vm            Chassis: vm         Machine ID: 20200914172502291423539425701795            Boot ID: bbf9cdd0b3fc4f85802668645a0c7bd6     Virtualization: kvm   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  19:44:24 up 4 days, 19:54,  0 users,  load average: 0.26, 0.14, 0.07

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -7,7 +7,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   ccsr[0x341]._32=epc;
   ccsr[0x342]._32=NO;
   ccsr[0x300]._32=0x1800;
-  printf("etrace: pc=%u cause=%u status=%u\n",ccsr[0x341]._32,ccsr[0x342]._32,ccsr[0x300]._32);
+  //printf("etrace: pc=%u cause=%u status=%u\n",ccsr[0x341]._32,ccsr[0x342]._32,ccsr[0x300]._32);
   /*for(int i=0;i<32;i++)
     printf("%u ",cpu.gpr[i]._32);
   printf("\n");*/
